2d8a233c1090f68819223cea313a37dbac737739
magicK5 <magichorus5@gmail.com>
2021-12-12 17:25:18 -0800

    mongodb success form web.

--- NodejsMongodbLog.js
+++ NodejsMongodbLog.js
@@ -1,8 +1,7 @@
 const express = require("express")
 const app =express()
 const mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/test1');
-
+mongoose.connect('mongodb://172.21.2.236:27017/190110910834');
 
 const schema={
     name: String,
@@ -11,12 +10,15 @@ const schema={
     hobby:String
 }
 const mydata = mongoose.model('cat1s', schema);
-const kitty = new mydata({ name: 'testZildjian'});
-kitty.save()
+// const kitty = new mydata({ name: 'testZildjian'});
+// kitty.save()
 
 app.use('/',express.static('public'))
 app.get("/input",(req,res)=>{
     res.send(req.query)
     console.log(req.query)
+
+    const kitty = new mydata({ name: req.query.first,health: req.query.second});
+    kitty.save()
 })
 app.listen(10834)
